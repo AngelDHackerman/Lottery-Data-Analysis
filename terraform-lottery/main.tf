3ae8bfb946e393f1c47189e097e7f9c1aa914b48
Angel Hernandez <angeldariaux@gmail.com>
2025-01-22 21:01:58 -0600

    added password policy and some usefull comments

--- terraform-lottery/main.tf
+++ terraform-lottery/main.tf
@@ -3,6 +3,7 @@ resource "aws_iam_group" "etl_group" {
   name = "ETLGroup"
 }
 
+# Create User group policy as ETLPolicy
 resource "aws_iam_group_policy" "etl_policy" {
   name  = "ETLPolicy"
   group = aws_iam_group.etl_group.name
@@ -35,4 +36,70 @@ resource "aws_iam_group_policy" "etl_policy" {
       }
     ]
   })
+}
+
+# Mandatory use of MFA for the IAM users
+resource "aws_iam_policy" "force_mfa" {
+  name        = "ForceMFA"
+  description = "Forzar el uso de MFA en IAM"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Deny"
+        Action = "*"
+        Resource = "*"
+        Condition = {
+          BoolIfExists = {
+            "aws:MultiFactorAuthPresent" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "force_mfa_attachment" {
+  group      = aws_iam_group.etl_group.name
+  policy_arn = aws_iam_policy.force_mfa.arn
+}
+
+# Restricted IPs address, only the known IPs will be able to access
+resource "aws_iam_policy" "restrict_ip_access" {
+  name        = "RestrictIPAccess"
+  description = "Restringir acceso de IAM a direcciones IP permitidas"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Deny"
+        Action = "*"
+        Resource = "*"
+        Condition = {
+          NotIpAddress = {
+            "aws:SourceIp" = var.allowed_ip_ranges
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "restrict_ip_attachment" {
+  group      = aws_iam_group.etl_group.name
+  policy_arn = aws_iam_policy.restrict_ip_access.arn
+}
+
+
+# Password policy each 90 days password should change
+resource "aws_iam_account_password_policy" "password_policy" {
+  minimum_password_length        = 12
+  require_lowercase_characters   = true
+  require_uppercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  max_password_age               = 90
 }
