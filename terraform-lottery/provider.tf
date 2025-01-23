05a89100d23f2b443fba41a06b431c359228f202
Angel Hernandez <angeldariaux@gmail.com>
2025-01-23 16:56:27 -0600

    removed single folder for both environments, prod and dev

--- terraform-lottery/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region     = var.aws_region
-
-  access_key = var.environment == "prod" ? var.prod_aws_access_key : var.dev_aws_access_key
-  secret_key = var.environment == "prod" ? var.prod_aws_secret_key : var.dev_aws_secret_key
-}
