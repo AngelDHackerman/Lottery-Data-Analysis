b6846ed9546bf3e683e4e018ef96bd28da136a5e
Angel Hernandez <angeldariaux@gmail.com>
2025-01-23 20:24:20 -0600

    added some comments

--- terraform-lottery/dev/secrets.tf
+++ terraform-lottery/dev/secrets.tf
@@ -1,3 +1,5 @@
+# Create a secret in AWS Secrets manager
+
 resource "aws_secretsmanager_secret" "lottery_secret" {
   name = "lottery_secret_${var.environment}"
   description = "Secrets for Lotería Santa Lucía ${var.environment}"
