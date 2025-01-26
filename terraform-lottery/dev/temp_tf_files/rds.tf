fd5f4941167101e4ecd55a7e38a0cba0d1afbab0
Angel Hernandez <angeldariaux@gmail.com>
2025-01-26 09:15:08 -0600

    added security group for the Database in network.tf

--- terraform-lottery/dev/temp_tf_files/rds.tf
+++ terraform-lottery/dev/temp_tf_files/rds.tf
@@ -9,19 +9,6 @@ resource "aws_db_instance" "lottery_db_dev" {
   password              = var.db_password_dev
   publicly_accessible   = false
   skip_final_snapshot   = true
-  db_subnet_group_name  = var.lottery_db_subent_group # need to create subnets 
+  db_subnet_group_name  = aws_db_subnet_group.lottery_db_subent_group.name # reference to the subnet group created in network.tf
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-}
-
-# Create a security group for the database
-resource "aws_security_group" "db_sg" {
-  name          = "lottery-db-sg-${var.environment}"
-  description   = "Security group for Lottery DB"
-
-  ingress = {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    cidr_blocks     = [var.public_ip]
-  }
 }
