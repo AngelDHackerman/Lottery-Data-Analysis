db46310dbc7fb4da43694603ef06c92940c234d8
Angel Hernandez <angeldariaux@gmail.com>
2025-02-02 15:24:13 -0600

    created EventBridge for execute StepFunctions weekly, Tuesday 12:30 PM UTC

--- terraform-lottery/dev/eventbridge.tf
+++ terraform-lottery/dev/eventbridge.tf
@@ -2,12 +2,12 @@
 resource "aws_cloudwatch_event_rule" "weekly_etl_execution" {
   name                  = "weekly-etl-execution-${var.environment}"
   description           = "Execute the ETL of Loteria Santa Lucia weekly"
-  schedule_expression   = "rate(7 days)"
+  schedule_expression   = "cron(0 12 ? * 3 *)" # Tuesday at 12:00 PM UTC
 }
 
 # Define the action of the rule, Execute Step Funtions
 resource "aws_cloudwatch_event_target" "trigger_step_functions" {
   rule          = aws_cloudwatch_event_rule.weekly_etl_execution.name
   arn           = aws_sfn_state_machine.lottery_etl_workflow.arn
-  role_arn      = aws_iam_role.eventbrige_role.arn  # crear role for event bridge
+  role_arn      = aws_iam_role.eventbridge_role.arn  # crear role for event bridge
 }
