af028e77c5946c0b86a65d19f6a6a42391bd75e8
Angel Hernandez <angeldariaux@gmail.com>
2025-06-10 18:55:26 -0600

    removed code rds.tf, rds instances no longer needed

--- terraform-lottery/Prod/network.tf
+++ terraform-lottery/Prod/network.tf
@@ -29,3 +29,32 @@ resource "aws_subnet" "private_subnet_2" {
     Name = "Private Subnet 2"
   }
 }
+
+# Internet Gateway 
+resource "aws_internet_gateway" "lottery_igw" {
+  vpc_id = aws_vpc.lottery_vpc.id
+
+  tags = {
+    Name = "lottery-igw-${var.environment}"
+  }
+}
+
+# Public Route Table 
+resource "aws_route_table" "lottery_public_rt" {
+  vpc_id = aws_vpc.lottery_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.lottery_igw.id
+  }
+
+  tags = {
+    Name = "lottery-public-rt-${var.environment}"
+  }
+}
+
+# Attach the route table to a subnet
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id       = aws_subnet.private_subnet_1.id
+  route_table_id  = aws_route_table.lottery_public_rt.id
+}
