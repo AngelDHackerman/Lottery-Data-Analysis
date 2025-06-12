00b7d7ae00236b986591fab4294c083b5fb399ce
Angel Hernandez <angeldariaux@gmail.com>
2025-06-11 22:24:33 -0600

    removed several iam policies no longer needed

--- terraform-lottery/Prod/iam.tf
+++ terraform-lottery/Prod/iam.tf
@@ -1,24 +1,5 @@
 # IAM Policies for Lambda, CloudTrail, CloudWatch and StepFuctions
 
-# Policy to allow Lambda functions to write logs into CloudWatch Logs
-resource "aws_iam_policy" "lambda-cloudwatch-policy" {
-  name        = "lottery-lambda-cloudwatch-policy-${var.environment}"
-  description = "Allows Lambda functions to write logs into CloudWatch Logs"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = [
-        "logs:CreateLogGroup", 
-        "logs:CreateLogStream", 
-        "logs:PutLogEvents"
-        ]
-      Resource = aws_cloudwatch_log_group.lambda_logs.arn
-    }]
-  })
-}
-
 # Policy to allow Lambda functions to access S3 buckets for the ETL .zip files
 resource "aws_iam_policy" "lambda-s3-policy" {
   name        = "lottery-lambda-s3-policy-${var.environment}"
@@ -52,107 +33,7 @@ resource "aws_iam_policy" "lambda-secrets-policy" {
   })
 }
 
-# Policy for CloudTrail to write logs in CloudWatch Logs
-resource "aws_iam_policy" "cloudtrail-cloudwatch-policy" {
-  name        = "lottery-cloudtrail-cloudwatch-policy-${var.environment}"
-  description = "Allows CloudTrail to send logs to CloudWatch Logs"
 
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams"
-        ]
-        Resource = "${aws_cloudwatch_log_group.cloudtrail_logs.arn}:*"
-      }
-    ]
-  })
-}
-
-# Policy to allow CloudTrail to store logs in an S3 bucket
-resource "aws_iam_policy" "cloudtrail-s3-policy" {
-  name        = "lottery-cloudtrail-s3-policy-${var.environment}"
-  description = "Allows CloudTrail to store logs in the designated S3 bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["s3:PutObject"]
-      Resource = "${aws_s3_bucket.cloudtrail_logs.arn}/*"
-    }]
-  })
-}
-
-# # Policy for StepFunctions to execute lambdas of the ETL
-# resource "aws_iam_policy" "step_functions_lambda_policy" {
-#   name          = "step_functions_lambda_policy_${var.environment}"
-#   description   = "Allows step functions to execute lambdas of ETL"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect      = "Allow"
-#       Action      = ["lambda:InvokeFunction"]  // Allows lambda invocation
-#       Resource    = [
-#         aws_lambda_function.scraper_lambda.arn,
-#         aws_lambda_function.transform_lambda.arn,
-#         aws_lambda_function.load_lambda.arn
-#       ]
-#     }]
-#   })
-# }
-
-# Policy: Allow StepFunctions to write logs in CloudWatch logs 
-resource "aws_iam_policy" "step_functions_logs_policy" {
-  name        = "step_functions_logs_policy_${var.environment}"
-  description = "Allows StepFunctions to write logs in CloudWatch"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        # Required for 'log delivery' setup at account level
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogDelivery",
-          "logs:GetLogDelivery",
-          "logs:UpdateLogDelivery",
-          "logs:DeleteLogDelivery",
-          "logs:ListLogDeliveries",
-          "logs:PutResourcePolicy",
-          "logs:DescribeResourcePolicies",
-          "logs:DescribeLogGroups"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogStreams"
-        ]
-        Resource = "${aws_cloudwatch_log_group.step_functions_logs.arn}:*"
-      },
-      {
-        # Step Functions needs to describe log groups (usually at account level)
-        Effect    = "Allow",
-        Action    = [
-          "logs:DescribeLogGroups"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
 
 # Policy: SageMaker S3 read-only for parquet files
 resource "aws_iam_policy" "sagemaker_s3_read_policy" {
@@ -177,21 +58,6 @@ resource "aws_iam_policy" "sagemaker_s3_read_policy" {
   })
 }
 
-# # Policy that allows EventBridge to start Step Funtions
-# resource "aws_iam_policy" "eventbridge_step_functions_policy" {
-#   name        = "eventbridge_step_functions_policy_${var.environment}"
-#   description = "Allows execution of EventBridge weekly"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect   = "Allow"
-#       Action   = ["states:StartExecution"]
-#       Resource = aws_sfn_state_machine.lottery_etl_workflow.arn
-#     }]
-#   })
-# }
-
 # IAM Role for Lambda functions
 resource "aws_iam_role" "lambda-role" {
   name = "lottery-lambda-role-${var.environment}"
@@ -202,21 +68,6 @@ resource "aws_iam_role" "lambda-role" {
     Statement = [{
       Effect    = "Allow"
       Principal = { Service = "lambda.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-# IAM Role for CloudTrail logging
-resource "aws_iam_role" "cloudtrail-logging-role" {
-  name = "lottery-cloudtrail-logging-role-${var.environment}"
-
-  # Allows AWS CloudTrail service to assume this role
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "cloudtrail.amazonaws.com" }
       Action    = "sts:AssumeRole"
     }]
   })
@@ -290,7 +141,8 @@ resource "aws_iam_policy" "sagemaker_studio_admin_policy" {
           "sagemaker:DescribeUserProfile",
           "sagemaker:DescribeSpace",
           "sagemaker:AddTags",
-          "sagemaker:CreateSpace" 
+          "sagemaker:CreateSpace",
+          "sagemaker:UpdateSpace"
         ],
         Resource = "*"
       }
@@ -300,12 +152,6 @@ resource "aws_iam_policy" "sagemaker_studio_admin_policy" {
 
 
 # Attach policies to IAM Roles
-
-# Attach policy for Lambda to write logs in CloudWatch
-resource "aws_iam_role_policy_attachment" "lambda-cloudwatch-attach" {
-  role       = aws_iam_role.lambda-role.name
-  policy_arn = aws_iam_policy.lambda-cloudwatch-policy.arn
-}
 
 # Attach policy for Lambda to access S3
 resource "aws_iam_role_policy_attachment" "lambda-s3-attach" {
@@ -317,24 +163,6 @@ resource "aws_iam_role_policy_attachment" "lambda-s3-attach" {
 resource "aws_iam_role_policy_attachment" "lambda-secrets-attach" {
   role       = aws_iam_role.lambda-role.name
   policy_arn = aws_iam_policy.lambda-secrets-policy.arn
-}
-
-# Attach policy for CloudTrail to write logs in S3
-resource "aws_iam_role_policy_attachment" "cloudtrail-s3-attach" {
-  role       = aws_iam_role.cloudtrail-logging-role.name
-  policy_arn = aws_iam_policy.cloudtrail-s3-policy.arn
-}
-
-# Attach policy for CloudTrail to write logs in CloudWatch
-resource "aws_iam_role_policy_attachment" "cloudtrail-cloudwatch-attach" {
-  role       = aws_iam_role.cloudtrail-logging-role.name
-  policy_arn = aws_iam_policy.cloudtrail-cloudwatch-policy.arn
-}
-
-# Attach StepFunctions to write logs in CloudWatch policy to StepFunctions Role
-resource "aws_iam_role_policy_attachment" "step_functions_logs_attach" {
-  policy_arn  = aws_iam_policy.step_functions_logs_policy.arn
-  role        = aws_iam_role.step_functions_role.name
 }
 
 # Attach policy to SageMaker execution role
