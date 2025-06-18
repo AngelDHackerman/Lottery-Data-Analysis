31c7762abbeb917e9c2df80279bea4207f7820eb
Angel Hernandez <angeldariaux@gmail.com>
2025-06-17 19:19:55 -0600

    redeployed 2 private subnets and 1 public on network.tf

--- terraform-lottery/Prod/network.tf
+++ terraform-lottery/Prod/network.tf
@@ -1,6 +1,30 @@
+# Create main VPC for Lottery Proyect
 resource "aws_vpc" "lottery" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = { Name = "lottery-vpc-${var.environment}" }
+}
+
+# Subnets 2 private for SageMaker Studio, 1 public for NAT Gateway
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.lottery.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = { Name = "priv-subnet-a" }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.lottery.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = { Name = "priv-subnet-b" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.lottery.id
+  cidr_block        = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = { Name = "pub-subnet" }
 }
