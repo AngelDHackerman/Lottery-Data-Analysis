0d00be8a19ca74b1182e9295f3df78c27d6f935e
Angel Hernandez <angeldariaux@gmail.com>
2025-06-20 15:56:02 -0600

    refactore sagemaker.tf to work with new vpc and new private subnets

--- terraform-lottery/Prod/sagemaker.tf
+++ terraform-lottery/Prod/sagemaker.tf
@@ -1,43 +1,42 @@
-# # -----------------------------
-# # SageMaker Studio Domain
-# # -----------------------------
-# resource "aws_sagemaker_domain" "lottery_domain" {
-#   domain_name               = "lottery-sagemaker-${var.environment}"
-#   auth_mode                 = "IAM"
-#   vpc_id                    = aws_vpc.lottery_vpc.id
+# -----------------------------
+# SageMaker Studio Domain
+# -----------------------------
+resource "aws_sagemaker_domain" "lottery_domain" {
+  domain_name               = "lottery-sagemaker-${var.environment}"
+  auth_mode                 = "IAM"
+  vpc_id                    = aws_vpc.lottery.id
+  app_network_access_type   = "VpcOnly"
 
-#   # Usa ambas subredes privadas
-#   subnet_ids                = [
-#     aws_subnet.private_subnet_1.id,
-#     aws_subnet.private_subnet_2.id
-#   ]
+  # Attach both private subnets where S3 endpoint was injected
+  subnet_ids        = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
 
-#   # Requiere acceso solo dentro del VPC (no usa internet directamente)
-#   app_network_access_type   = "VpcOnly"
+  default_user_settings {
+    execution_role    = var.lottery_sagemaker_execution_role_prod_arn
+    security_groups   = [ aws_security_group.sagemaker_studio.id ]  # attach the security group
+    
 
-#   default_user_settings {
-#     execution_role = var.lottery_sagemaker_execution_role_prod_arn
+    jupyter_server_app_settings {
+      default_resource_spec {
+        instance_type = "system"
+      }
+    }
+  } 
+  tags = {
+    Name = "lottery-sagemaker-${var.environment}"
+  }
+}
 
-#     jupyter_server_app_settings {
-#       default_resource_spec {
-#         instance_type = "system"
-#       }
-#     }
-#   }
+# -----------------------------
+# SageMaker User Profile
+# -----------------------------
+resource "aws_sagemaker_user_profile" "lottery_user" {
+  domain_id         = aws_sagemaker_domain.lottery_domain.id
+  user_profile_name = "lottery-analyst"
 
-#   tags = {
-#     Name = "lottery-sagemaker-${var.environment}"
-#   }
-# }
-
-# # -----------------------------
-# # SageMaker User Profile
-# # -----------------------------
-# resource "aws_sagemaker_user_profile" "lottery_user" {
-#   domain_id         = aws_sagemaker_domain.lottery_domain.id
-#   user_profile_name = "lottery-analyst"
-
-#   user_settings {
-#     execution_role = var.lottery_sagemaker_execution_role_prod_arn
-#   }
-# }
+  user_settings {
+    execution_role = var.lottery_sagemaker_execution_role_prod_arn
+  }
+}
