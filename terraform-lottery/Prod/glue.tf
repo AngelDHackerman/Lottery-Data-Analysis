29b615c912dedb353ecc4e4e1c43103b91ef7442
Angel Hernandez <angeldariaux@gmail.com>
2025-07-08 22:30:51 -0600

    created 2 crawlers for s3 buckets

--- terraform-lottery/Prod/glue.tf
+++ terraform-lottery/Prod/glue.tf
@@ -2,3 +2,65 @@
 resource "aws_glue_catalog_database" "lottery_db" {
   name = "lottery_santalucia_db"
 }
+
+# Crawler pointing to partitioned S3 bucket "Premios" section
+resource "aws_glue_crawler" "premios_crawler" {
+  name              = "lottery-premios-crawler"
+  role              = aws_iam_role.glue_crawler_role.arn
+  database_name     = aws_glue_catalog_database.lottery_db.name
+  table_prefix      = "premios_"
+
+  s3_target {
+    path = "s3://lottery-partitioned-storage-prod/processed/premios/"
+  }
+
+   configuration = jsonencode({
+    Version = 1.0,
+    CrawlerOutput = {
+      Partitions = {
+        AddOrUpdateBehavior = "InheritFromTable"
+      }
+    }
+  })
+
+  schema_change_policy {
+    delete_behavior = "LOG"
+    update_behavior = "LOG"
+  }
+
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_NEW_FOLDERS_ONLY"
+  }
+
+}
+
+# Crawler pointing to partitioned S3 bucket "Sorteos" section
+resource "aws_glue_crawler" "sorteos_crawler" {
+  name          = "lottery-sorteos-crawler"
+  role          = aws_iam_role.glue_crawler_role.arn
+  database_name = aws_glue_catalog_database.lottery_db.name
+  table_prefix  = "sorteos_"
+
+  s3_target {
+    path = "s3://lottery-partitioned-storage-prod/processed/sorteos/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0,
+    CrawlerOutput = {
+      Partitions = {
+        AddOrUpdateBehavior = "InheritFromTable"
+      }
+    }
+  })
+
+  schema_change_policy {
+    delete_behavior = "LOG"
+    update_behavior = "LOG"
+  }
+
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_NEW_FOLDERS_ONLY"
+  }
+
+}
