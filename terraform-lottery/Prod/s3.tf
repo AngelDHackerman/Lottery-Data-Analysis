8ac36513069d6bf25ca0d1d81dd90c592a901d1c
Angel Hernandez <angeldariaux@gmail.com>
2025-07-11 20:54:11 -0600

    creted bucket for Athena results

--- terraform-lottery/Prod/s3.tf
+++ terraform-lottery/Prod/s3.tf
@@ -27,3 +27,44 @@
 #     Project     = "Lottery ETL"
 #   }
 # }
+
+# Bucket for Athena results
+resource "aws_s3_bucket" "athena_results" {
+  bucket            = "lottery-athena-results-${var.environment}"
+  force_destroy     = true
+  tags = {
+    Name = "Athena Query Resutls"
+  }
+}
+
+# Block public access 
+resource "aws_s3_bucket_public_access_block" "athena_results" {
+  bucket                    = aws_s3_bucket.athena_results.id
+  block_public_acls         = true
+  block_public_policy       = true
+  ignore_public_acls        = true
+  restrict_public_buckets   = true
+}
+
+# Encryption using SSE-S3
+resource "aws_s3_bucket_server_side_encryption_configuration" "athena_results" {
+  bucket = aws_s3_bucket.athena_results.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Remove results after 30 days
+resource "aws_s3_bucket_lifecycle_configuration" "athena_results" {
+  bucket = aws_s3_bucket.athena_results.id
+  rule {
+    id      = "expire-athena-results"
+    status  =  "Enabled"
+    filter {}
+    expiration {
+      days = 30
+    }
+  } 
+}
