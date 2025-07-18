8bb51791f64713975827dc2e3eb7a3b8b3c7557c
Angel Hernandez <angeldariaux@gmail.com>
2025-07-17 20:38:16 -0600

    added some init python to file structure

---
+++ lambda/transformer/lambda_handler.py
@@ -0,0 +1,24 @@
+from transformer import transform
+from extractor.secrets import get_secrets
+
+def lambda_handler(event, context):
+    """
+    AWS Lambda entrypoint for triggering the transformation process.
+    """
+    print("🔁 Trigger received in Lambda...")
+
+    # Get bucket info
+    secrets = get_secrets()
+    partitioned_bucket = secrets["partitioned"]
+
+    raw_prefix = "raw/"
+    processed_prefix = "processed/"
+
+    print(f"🎯 Starting transformation in bucket: {partitioned_bucket}")
+    transform(partitioned_bucket, raw_prefix=raw_prefix, processed_prefix=processed_prefix)
+    print("✅ Transformation completed.")
+
+    return {
+        "statusCode": 200,
+        "body": "Transformation completed successfully"
+    }
--- lambda/transformer/transformer.py
+++ lambda/transformer/transformer.py
@@ -5,7 +5,7 @@ import pandas as pd
 import boto3
 from botocore.exceptions import ClientError
 
-from transformer.parser import (
+from .parser import (
     split_header_body,
     process_header,
     process_body,
