4c80b3f0a8b8bfc149b58b279e942b28fc6a5548
Angel Hernandez <angeldariaux@gmail.com>
2025-07-21 15:53:46 -0600

    role for lambdas and basic lambda access to cloudWatch

--- terraform-lottery/Prod/iam.tf
+++ terraform-lottery/Prod/iam.tf
@@ -136,6 +136,25 @@ resource "aws_iam_role" "glue_crawler_role" {
   })
 }
 
+# Role for Lambdas
+resource "aws_iam_role" "lambda_exec" {
+  name = "lottery-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect      = "Allow",
+      Principal   = { Service = "lambda.amazonaws.com" },
+      Action      = "sts:AssumenRole"
+    }]
+  })
+}
+
+# Lambda Basic access for CloudWatch Logs
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
 # Attach policy to SageMaker execution role
 resource "aws_iam_role_policy_attachment" "sagemaker_s3_read_attach" {
--- terraform-lottery/Prod/lambdas.tf
+++ terraform-lottery/Prod/lambdas.tf
@@ -1,45 +1,45 @@
-# Lambda: Extractor 
-resource "aws_lambda_function" "extractor_lambda" {
-  function_name         = "lottery-extractor"
-  handler               = "extractor.lambda_handler.lambda_hadler"
-  runtime               = "python3.12"
-  timeout               = 120
-  memory_size           = 1024
-  filename              = var.lambdas_path_local
-  source_code_hash      = filebase64sha256(var.lambdas_path_local)
+# # Lambda: Extractor 
+# resource "aws_lambda_function" "extractor_lambda" {
+#   function_name         = "lottery-extractor"
+#   handler               = "extractor.lambda_handler.lambda_hadler"
+#   runtime               = "python3.12"
+#   timeout               = 120
+#   memory_size           = 1024
+#   filename              = var.lambdas_path_local
+#   source_code_hash      = filebase64sha256(var.lambdas_path_local)
 
-  role = aws_iam_role.lambda_exec.arn
+#   role = aws_iam_role.lambda_exec.arn
 
-  environment {
-    variables = {
-      PARTITIONED_BUCKET    = var.s3_bucket_partitioned_data_storage_prod_arn
-      SIMPLE_BUCKET         = var.s3_bucket_simple_data_storage_prod_arn
-      REGION                = var.aws_region
-    }
-  }
+#   environment {
+#     variables = {
+#       PARTITIONED_BUCKET    = var.s3_bucket_partitioned_data_storage_prod_arn
+#       SIMPLE_BUCKET         = var.s3_bucket_simple_data_storage_prod_arn
+#       REGION                = var.aws_region
+#     }
+#   }
 
-  depends_on = [ aws_iam_role_policy_attachment.lambda_basic ]
-}
+#   depends_on = [ aws_iam_role_policy_attachment.lambda_basic ]
+# }
 
-# Lambda: Transformer
-resource "aws_lambda_function" "transformer_lambda" {
-  function_name    = "lottery-transformer"
-  handler          = "transformer.lambda_handler.lambda_handler"
-  runtime          = "python3.12"
-  timeout          = 180
-  memory_size      = 1024
-  filename         = var.lambdas_path_local
-  source_code_hash = filebase64sha256(var.lambdas_path_local)
+# # Lambda: Transformer
+# resource "aws_lambda_function" "transformer_lambda" {
+#   function_name    = "lottery-transformer"
+#   handler          = "transformer.lambda_handler.lambda_handler"
+#   runtime          = "python3.12"
+#   timeout          = 180
+#   memory_size      = 1024
+#   filename         = var.lambdas_path_local
+#   source_code_hash = filebase64sha256(var.lambdas_path_local)
 
-  role = aws_iam_role.lambda_exec.arn
+#   role = aws_iam_role.lambda_exec.arn
 
-  environment {
-    variables = {
-      PARTITIONED_BUCKET = var.s3_bucket_partitioned_data_storage_prod_arn
-      SIMPLE_BUCKET      = var.s3_bucket_simple_data_storage_prod_arn
-      REGION             = var.aws_region
-    }
-  }
+#   environment {
+#     variables = {
+#       PARTITIONED_BUCKET = var.s3_bucket_partitioned_data_storage_prod_arn
+#       SIMPLE_BUCKET      = var.s3_bucket_simple_data_storage_prod_arn
+#       REGION             = var.aws_region
+#     }
+#   }
 
-  depends_on = [aws_iam_role_policy_attachment.lambda_basic]
-}
+#   depends_on = [aws_iam_role_policy_attachment.lambda_basic]
+# }
